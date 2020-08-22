751cc7a92965bbb6e41e80dbe99d563941bd3785
guoyq16 <regenmelodie@163.com>
2020-08-22 11:28:57 +0800

    write scap

---
+++ Scapple_write_xml/Scapple_write_xml/Program.cs
@@ -0,0 +1,206 @@
+﻿using System;
+
+namespace Scapple_write_xml
+{
+    class WriteXML
+    {
+        private void WriteToScap(string str)
+        {
+            Console.Write(str);
+            return;
+        }
+
+        public void ScappleDocument()
+        {
+            WriteToScap("<ScappleDocument>\n");
+            Notes();
+            WriteToScap("\n");
+            UISettings();
+            WriteToScap("</ScappleDocument>\n");
+            return;
+        }
+
+        private void Notes()
+        {
+            WriteToScap("<Notes>\n");
+            Note();
+            WriteToScap("</Notes>\n");
+            return;
+        }
+
+        private void Note()
+        {
+            double width;
+            int fontSize;
+            int id;
+            double position_x, position_y;
+
+            width = 52.2827;
+            fontSize = 13;
+            id = 0;
+            position_x = 331.2;
+            position_y = 202.0;
+
+            string widthString = "Width='" + width.ToString() + "' ";
+            string fontSizeString = "FontSize='" + fontSize.ToString() + "' ";
+            string idString = "ID='" + id.ToString() + "' ";
+            string positionString = "Position='" + position_x.ToString() + "," + position_y.ToString() + "'";
+
+            string str = "<Note " + widthString + fontSizeString + idString + positionString + ">\n";
+            WriteToScap(str);
+
+            Appearance();
+            String();
+
+            WriteToScap("</Note>\n");
+            return;
+        }
+
+        private void Appearance()
+        {
+            WriteToScap("<Appearance>\n");
+            Alignment();
+            TextColor();
+            Fill();
+            Border();
+            WriteToScap("</Appearance>\n");
+            return;
+        }
+
+        private void Alignment()
+        {
+            WriteToScap("<Alignment>");
+            string str = "Left";
+            WriteToScap(str);
+            WriteToScap("</Alignment>\n");
+            return;
+        }
+
+        private void TextColor()
+        {
+            WriteToScap("<TextColor>");
+            double a, b, c;
+
+            a = 0.666667;
+            b = 0.666667;
+            c = 1.0;
+
+            string str = a.ToString() + " " + b.ToString() + " " + c.ToString();
+            WriteToScap(str);
+            WriteToScap("</TextColor>\n");
+            return;
+        }
+
+        private void Fill()
+        {
+            WriteToScap("<Fill>");
+            double a, b, c;
+
+            a = 0.941176;
+            b = 1.0;
+            c = 0.396078;
+
+            string str = a.ToString() + " " + b.ToString() + " " + c.ToString();
+            WriteToScap(str);
+            WriteToScap("</Fill>\n");
+            return;
+        }
+
+        private void Border()
+        {
+            double weight;
+            string style;
+
+            weight = 7;
+            style = "Rounded";
+
+            string weightString = "Weight='" + weight.ToString() + "' ";
+            string styleString = "Style='" + style + "'";
+
+            string str1 = "<Border " + weightString + styleString + ">";
+            WriteToScap(str1);
+
+            double a, b, c;
+
+            a = 0.0;
+            b = 1.0;
+            c = 1.0;
+
+            string str2 = a.ToString() + " " + b.ToString() + " " + c.ToString();
+            WriteToScap(str2);
+
+            WriteToScap("</Border>\n");
+            return;
+        }
+
+        private void String()
+        {
+            WriteToScap("<String>");
+            string str = "new";
+            WriteToScap(str);
+            WriteToScap("</String>\n");
+            return;
+        }
+
+
+        private void UISettings()
+        {
+            WriteToScap("<UISettings>\n");
+            BackgroundColor();
+            DefaultFont();
+            DefaultTextColor();
+            WriteToScap("</UISettings>\n");
+            return;
+        }
+
+        private void BackgroundColor()
+        {
+            WriteToScap("<BackgroundColor>");
+
+            double a, b, c;
+            a = 1.0;
+            b = 0.988006;
+            c = 0.945006;
+
+            string str = a.ToString() + " " + b.ToString() + " " + c.ToString();
+            WriteToScap(str);
+
+            WriteToScap("</BackgroundColor>\n");
+            return;
+        }
+
+        private void DefaultFont()
+        {
+            WriteToScap("<DefaultFont>");
+            string str;
+            str = "Helvetica";
+            WriteToScap(str);
+            WriteToScap("</DefaultFont>\n");
+            return;
+        }
+
+        private  void DefaultTextColor()
+        {
+            WriteToScap("<DefaultTextColor>");
+            double a, b, c;
+            a = 0.0;
+            b = 0.0;
+            c = 0.0;
+            string str = a.ToString() + " " + b.ToString() + " " + c.ToString();
+            WriteToScap(str);
+            WriteToScap("</DefaultTextColor>\n");
+            return;
+        }
+
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WriteXML writeXML = new WriteXML();
+            writeXML.ScappleDocument();
+            Console.ReadKey();
+        }
+    }
+}
--- parameter_test.py
+++ parameter_test.py
@@ -2,28 +2,51 @@ from xml.dom.minidom import parse
 import xml.dom.minidom
 
 # 使用minidom解析器打开 XML 文档
-DOMTree = xml.dom.minidom.parse("C:/Users/Regen/Desktop/Scapple-learning/xmlFiles/test1.xml")
+DOMTree = xml.dom.minidom.parse("C:/Users/Regen/Desktop/Scapple-learning/xmlFiles/test1.scap")
 scappleDocument = DOMTree.documentElement
 if scappleDocument.hasAttribute("ID"):
     print("Root element : %s" % scappleDocument.getAttribute("ID"))
 
-# 在集合中获取所有<Notes>
-wnotes = scappleDocument.getElementsByTagName("Notes")
+if scappleDocument.getElementsByTagName("Notes"): # <ScappleDocument>中有<Notes>
+    wnotes = scappleDocument.getElementsByTagName("Notes") # 在集合中获取所有<Notes>
 
-# 在集合中获取所有<Note>
-for _n in wnotes:
-    notes = _n.getElementsByTagName("Note")
-    print("*****Notes*****")
+    # 遍历集合中所有<Note>
+    for _n in wnotes:
+        if _n.getElementsByTagName("Note"): # <Notes>中有<Note>
+            notes = _n.getElementsByTagName("Note")
+            print("***Notes:")
 
-    for note in notes:
-        print("[Note]")
+            for note in notes:
+                print("******Note:")
 
-        if note.hasAttribute("Width"):
-            print("Width: %s" % note.getAttribute("Width"))
-        if note.hasAttribute("FontSize"):
-            print("FontSize: %s" % note.getAttribute("FontSize"))
-        if note.hasAttribute("Position"):
-            print("Position: %s" % note.getAttribute("Position"))
+                if note.hasAttribute("Width"):
+                    print("******(Width: %s" % note.getAttribute("Width"))
+                if note.hasAttribute("FontSize"):
+                    print("******(FontSize: %s" % note.getAttribute("FontSize"))
+                if note.hasAttribute("Position"):
+                    print("******(Position: %s" % note.getAttribute("Position"))
 
-# 打印每个Note的详细信息
+                if note.getElementsByTagName("Appearance"):
+                    appearance = note.getElementsByTagName('Appearance')[0]
+                    print("*********Appearance:")
+                    if appearance.getElementsByTagName("Alignment"):
+                        alignment = appearance.getElementsByTagName('Alignment')[0]
+                        print("************Alignment: %s" % alignment.childNodes[0].data)
+                    if appearance.getElementsByTagName("TextColor"):
+                        textColor = appearance.getElementsByTagName('TextColor')[0]
+                        print("************TextColor: %s" % textColor.childNodes[0].data)
+                    if appearance.getElementsByTagName("Border"):
+                        border = appearance.getElementsByTagName('Border')[0]
+                        print("************Border:")
+                        if border.hasAttribute("Weight"):
+                            print("************(Weight: %s" % border.getAttribute("Weight"))
+                        if border.hasAttribute("Style"):
+                            print("************(Style: %s" % border.getAttribute("Style"))
+                
+                if note.getElementsByTagName("String"):
+                    string = note.getElementsByTagName('String')[0]
+                    print("*********String: %s" % string.childNodes[0].data)
 
+                if note.getElementsByTagName("ConnectedNoteIDs"):
+                    connectedNoteIDs = note.getElementsByTagName('ConnectedNoteIDs')[0]
+                    print("*********ConnectedNoteIDs: %s" % connectedNoteIDs.childNodes[0].data)
